82e4cf1f3a6bcfb00c86d8f923e4065bbb247d47
Peter Lieverdink <peter.lieverdink@unimelb.edu.au>
2013-08-06 16:54:35 +1000

    Do supervision queries and theming.

--- templates/faeprofile-supervision.tpl.php
+++ templates/faeprofile-supervision.tpl.php
@@ -4,4 +4,13 @@
  */
 ?>
 
-Supervision.
+<h3 class="title">Supervision</h3>
+
+<?php if ($available): ?>
+  <?php print $available; ?> for supervision.
+<?php endif; ?>
+
+<?php if ($overview): ?>
+  <h4>Supervision Statement</h4>
+  <p><?php print $overview; ?></p>
+<?php endif; ?>
