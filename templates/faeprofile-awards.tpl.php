abf1e48ceacc985bc3034398988ddbbc0cd0cd48
Peter Lieverdink <peter.lieverdink@unimelb.edu.au>
2013-08-20 11:03:43 +1000

    Add preprocess and theme functions for affiliations, awards, linkages and research. Extra option to add information to main profile.

--- templates/faeprofile-awards.tpl.php
+++ templates/faeprofile-awards.tpl.php
@@ -1,7 +1,21 @@
 <?php
 /**
  * @file
+ *
+ * Available template variables are:
+ *
+ * $awards - An array of all awards.
+ * $year   - An array of years, each of which contains an array of awards
+ *           for that year.
  */
 ?>
 
-Awards.
+<?php if ($awards): ?>
+  <h3>Awards and honours</h3>
+  <ul>
+  <?php foreach ($awards as $award): ?>
+    <li><?php print $award; ?></li>
+  <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
+
