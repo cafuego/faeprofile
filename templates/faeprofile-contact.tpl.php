afd352d65df6811bd868ffbaed494393928c7f21
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-08-26 16:56:19 +1000

    staff overview contains overviewText1, overviewText2, overviewText3, overviewText4 in find an expert website

--- templates/faeprofile-contact.tpl.php
+++ templates/faeprofile-contact.tpl.php
@@ -11,5 +11,5 @@
 <?php if ($email): ?><strong>Email:</strong> <?php print $email; ?><br /><?php endif; ?>
 <?php if ($phone): ?><strong>Phone:</strong> <?php print $phone; ?><br /><?php endif; ?>
 <?php if ($webpage): ?><strong>Website:</strong> <?php print $webpage; ?><br /><?php endif; ?>
-<?php if (FALSE && ($room || $building)): ?><strong>Location:</strong> <?php print $room; ?>  <?php print $building; ?><br /><?php endif; ?>
+<?php if ($room || $building): ?><strong>Location:</strong> <?php print $room; ?>  <?php print $building; ?><br /><?php endif; ?>
 <?php if ($supervisor): ?><strong>Supervision:</strong> <?php print $supervisor; ?><?php endif; ?>
