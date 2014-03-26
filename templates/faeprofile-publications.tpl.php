1d3dba9d09f1f7445e36d2373dcc061fbf18d893
arts-webmaster <web.arts.unimelb@gmail.com>
2014-03-26 16:58:25 +1100

    Able to sort by pub_type. Todo: have an ordered list; sort by article title

--- templates/faeprofile-publications.tpl.php
+++ templates/faeprofile-publications.tpl.php
@@ -10,14 +10,23 @@
  */
 ?>
 
-<?php if ($year): ?>
+<?php if($year): ?>
   <h3>Selected publications</h3>
-  <?php foreach ($year as $label => $publications): ?>
-     <h4><?php print $label; ?></h4>
-     <ul>
-     <?php foreach ($publications as $publication): ?>
-       <li><?php print $publication; ?></li>
-    <?php endforeach; ?>
-    </ul>
-  <?php endforeach; ?>
+	<?php foreach($year as $year_label => $pub_types): ?>  
+		<h4><?php print $year_label; ?></h4>
+
+		<?php
+			uksort($pub_types, '_my_pub_type_compare');
+		?>
+
+		<?php foreach($pub_types as $pub_type_label => $pub_type): ?>	
+			<h5><?php print $pub_type_label ?></h5>
+			<ul>
+			<?php foreach($pub_type as $single_pub): ?>
+				<li><?php print $single_pub ?></li>
+			<?php endforeach; ?>
+			</ul>
+		<?php endforeach; ?>
+	<?php endforeach; ?>
 <?php endif; ?>
+
