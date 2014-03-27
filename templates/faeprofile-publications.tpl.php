48a5bb0c3112df0fca61252e536897d4219fc11f
arts-webmaster <web.arts.unimelb@gmail.com>
2014-03-27 11:52:49 +1100

    Order publications by pub uri

--- templates/faeprofile-publications.tpl.php
+++ templates/faeprofile-publications.tpl.php
@@ -15,13 +15,14 @@
 	<?php foreach($year as $year_label => $pub_types): ?>  
 		<h4><?php print $year_label; ?></h4>
 
-		<?php
-			uksort($pub_types, '_my_pub_type_compare');
-		?>
+		<?php uksort($pub_types, '_my_pub_type_compare'); ?>
 
 		<?php foreach($pub_types as $pub_type_label => $pub_type): ?>	
 			<h5><?php print $pub_type_label ?></h5>
 			<ul>
+		
+			<?php ksort($pub_type); ?>
+		
 			<?php foreach($pub_type as $single_pub): ?>
 				<li><?php print $single_pub ?></li>
 			<?php endforeach; ?>
