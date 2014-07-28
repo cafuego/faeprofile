f24f4fd535ddbf1f4d972503f5f115817b427ee7
arts-unimelb <arts-webmaster@unimelb.edu.au>
2014-07-28 14:42:08 +1000

    add faeprofile_id to contact tag

--- templates/faeprofile-contact.tpl.php
+++ templates/faeprofile-contact.tpl.php
@@ -6,7 +6,7 @@
 
 <?php print $image; ?>
 
-<h3>Contact</h3>
+<h3 class="faeprofile-contact-<?php print $faeprofile_id; ?>">Contact</h3>
 
 <?php if ($email): ?><strong>Email:</strong> <?php print $email; ?><br /><?php endif; ?>
 <?php if ($phone): ?><strong>Phone:</strong> <?php print $phone; ?><br /><?php endif; ?>
