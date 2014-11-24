2c9345f933e6b4151adae60833ed1ed465f0c1a8
kenpeter <figo2476@hotmail.com>
2014-11-24 11:46:53 +1100

    Add contract info under research tab

--- templates/faeprofile-research.tpl.php
+++ templates/faeprofile-research.tpl.php
@@ -9,27 +9,53 @@
  */
 ?>
 
-<?php if(isset($year) && is_array($year) && count($year) > 0): ?>
+<?php $year = $contract['year']; ?>
+<?php if(count($year) > 0): ?>
+  <h3>Contract information</h3>
+  <?php foreach($year as $each_year_key => $each_year): ?>
+    <?php
+      if(empty($each_year_key)) {
+        $each_year_key_label = 'Other';
+      }
+      else {
+        $each_year_key_label = $each_year_key;
+      }
+    ?>
+    <h4><?php echo $each_year_key_label ?></h4>
+    <ul>
+      <?php foreach($each_year as $each_project): ?>
+        <li class="faeprofile-block-research-list-item"><?php echo $each_project ?></li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endforeach; ?>
+<?php endif; ?>
+
+
+<p>&nbsp;</p>
+<?php $year = $grant['year']; ?>
+<?php if(count($year) > 0): ?>
 	<h3>Grant information</h3>
 	<?php foreach($year as $each_year_key => $each_year): ?>
-		<?php 
-			if(empty($each_year_key)) {
-				$each_year_key_label = 'Other';
-			}
-			else {
-				$each_year_key_label = $each_year_key;
-			} 
-		?>
-		<h4><?php echo $each_year_key_label ?></h4>
-		<ul>
-			<?php foreach($each_year as $each_project): ?>
-				<li class="faeprofile-block-research-list-item"><?php echo $each_project ?></li>
-			<?php endforeach; ?>
-		</ul>
+		<?php
+      if(empty($each_year_key)) {
+        $each_year_key_label = 'Other';
+      }
+      else {
+        $each_year_key_label = $each_year_key;
+      }
+    ?>
+    <h4><?php echo $each_year_key_label ?></h4>
+    <ul>
+      <?php foreach($each_year as $each_project): ?>
+        <li class="faeprofile-block-research-list-item"><?php echo $each_project ?></li>
+      <?php endforeach; ?>
+    </ul>
 	<?php endforeach; ?>
 <?php endif; ?>
 
-<?php if(!empty($extra_grant_text)): ?>
-	<h3>Additional grant information</h3>
-  <?php echo $extra_grant_text ?>
+
+<?php $extra_text = $grant['extra_grant_text']; ?>
+<?php if(!empty($extra_text)): ?>
+  <h3>Additional grant information</h3>
+  <?php echo $extra_text ?>
 <?php endif; ?>
